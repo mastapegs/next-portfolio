7fef294b0f21e2650f3f345c4eb3a95bb732699c
Matthew Pagan <mastapegs01@gmail.com>
2020-03-25 15:37:55 -0400

    Fixing Nav

--- components/Header.js
+++ components/Header.js
@@ -13,27 +13,29 @@ const Header = () => {
     }
   ];
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand>
-        <Link href="/">
-          <a>Matthew Pagan</a>
-        </Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          {links.map(({ href, text }) => {
-            return (
-              <Nav.Link>
-                <Link href={href}>
-                  <a>{text}</a>
-                </Link>
-              </Nav.Link>
-            );
-          })}
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>
+          <Link href="/">
+            <a>Matthew Pagan</a>
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            {links.map(({ href, text }) => {
+              return (
+                <Nav.Item>
+                  <Link href={href}>
+                    <a>{text}</a>
+                  </Link>
+                </Nav.Item>
+              );
+            })}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
 }
 export default Header;
