7092aaf5f71facfba288f9f53171231ff465e52e
Matthew Pagan <mastapegs01@gmail.com>
2020-03-26 22:26:33 -0400

    Add state to <Form>, Create Alert message

--- pages/contact.js
+++ pages/contact.js
@@ -2,7 +2,22 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
+import { useState } from 'react'
+
 const Contact = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(name);
+    console.log(email);
+    console.log(message);
+  }
+
   return (
     <>
       <Jumbotron>
@@ -11,19 +26,42 @@ const Contact = () => {
           Your friendly neighborhood web developer 🕸️
         </p>
       </Jumbotron>
+      <Alert variant="warning">
+        <Alert.Heading>🚧 Still Implementing Back-end 🚧</Alert.Heading>
+        <p>
+          I'm still in the process of building out the back-end of this form to actually get your message from you, to me.
+        </p>
+      </Alert>
+      <hr />
       <Container>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group controlId="name">
             <Form.Label>Your Name</Form.Label>
-            <Form.Control type="text" placeholder="Your name" />
+            <Form.Control
+              type="text"
+              placeholder="Your name"
+              name="name"
+              value={name}
+              onChange={() => setName(event.target.value)} />
           </Form.Group>
           <Form.Group controlId="email">
             <Form.Label>Your Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              name="email"
+              value={email}
+              onChange={() => setEmail(event.target.value)} />
           </Form.Group>
           <Form.Group controlId="message">
             <Form.Label>Your Message</Form.Label>
-            <Form.Control as="textarea" row="4" placeholder="Your message" />
+            <Form.Control
+              as="textarea"
+              rows="4"
+              placeholder="Your message"
+              name="message"
+              value={message}
+              onChange={() => setMessage(event.target.value)} />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
