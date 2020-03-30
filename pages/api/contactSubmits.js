d106ea3d022f7d079aab36c1cf70b9d31d4e92f3
Matthew Pagan <mastapegs01@gmail.com>
2020-03-29 22:54:42 -0400

    Connect to Atlas via mongodb client

--- pages/api/contactSubmits.js
+++ pages/api/contactSubmits.js
@@ -1,33 +1,34 @@
-import mongoose from 'mongoose'
-import ContactSubmitSchema from '../../models/ContactSubmit'
-const ContactSubmit = mongoose.model("contactSubmits", ContactSubmitSchema);
 
-export default (req, res) => {
-  
+export default async (req, res) => {
+
   console.log(req.method);
   console.table(req.body);
   const { name, email, message } = req.body;
 
   console.log(process.env.ATLASURI);
-  mongoose.connect(process.env.ATLASURI, {useNewUrlParser: true});
-  const db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', function() {
-    console.log("Connected to Atlas");
 
-    // const newSubmit = new ContactSubmit({
-    //   name,
-    //   email,
-    //   message
-    // });
-    // console.log(newSubmit)
+  const MongoClient = require('mongodb').MongoClient;
 
-    // newSubmit.save((err)=>{
-    //   if (err) return err;
-    //   console.log("New Submission");
-    // });
+  await (async function () {
+    const dbName = 'next-portfolio';
+    const client = new MongoClient(process.env.ATLASURI, { useNewUrlParser: true });
 
-  });
+    try {
+      await client.connect();
+
+      const db = client.db(dbName);
+      let response = await db.collection('contactSubmits').insertOne({
+        name,
+        email,
+        message
+      });
+
+    } catch (err) {
+      console.log(err.stack);
+    }
+
+    client.close();
+  })();
 
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
