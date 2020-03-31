b76e32a7afc1af64a348e43307fe198cdec1a8e8
Matthew Pagan <mastapegs01@gmail.com>
2020-03-31 13:20:55 -0400

    Refactor code a bit

--- pages/api/contactSubmits.js
+++ pages/api/contactSubmits.js
@@ -1,36 +1,26 @@
-
 export default async (req, res) => {
+  if (req.method === "POST") {
+    const { name, email, message } = req.body;
+    const MongoClient = require('mongodb').MongoClient;
+    await (async () => {
+      const dbName = 'next-portfolio';
+      const client = new MongoClient(process.env.ATLASURI, { useNewUrlParser: true });
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        let response = await db.collection('contactSubmits').insertOne({
+          name,
+          email,
+          message
+        });
+      } catch (err) {
+        console.log(err.stack);
+      }
+      client.close();
+    })();
 
-  console.log(req.method);
-  console.table(req.body);
-  const { name, email, message } = req.body;
-
-  console.log(process.env.ATLASURI);
-
-  const MongoClient = require('mongodb').MongoClient;
-
-  await (async function () {
-    const dbName = 'next-portfolio';
-    const client = new MongoClient(process.env.ATLASURI, { useNewUrlParser: true });
-
-    try {
-      await client.connect();
-
-      const db = client.db(dbName);
-      let response = await db.collection('contactSubmits').insertOne({
-        name,
-        email,
-        message
-      });
-
-    } catch (err) {
-      console.log(err.stack);
-    }
-
-    client.close();
-  })();
-
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify(req.body))
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(req.body))
+  }
 }
